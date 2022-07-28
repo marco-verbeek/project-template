e5c355ba754f855714f8214ea838eea807d7397d
Marco VERBEEK <marcoverbeek99@gmail.com>
2022-07-28 20:36:16 +0200

    feat(REPL): added REPL functionality + improved Docker logic

--- apps/api/src/config/mongodb.config.ts
+++ apps/api/src/config/mongodb.config.ts
@@ -1,22 +1,20 @@
 import { registerAs } from '@nestjs/config';
 
-const isDevelopment = true;
-
 /**
  * Mongo database connection config
  */
 export default registerAs('mongodb', () => {
-  const {
-    MONGO_PORT,
-    MONGO_HOSTNAME,
-    MONGO_DATABASE,
-    MONGO_USERNAME,
-    MONGO_PASSWORD,
-  } = process.env;
+  const { MONGO_PORT, MONGO_DATABASE, MONGO_USERNAME, MONGO_PASSWORD } =
+    process.env;
+
+  const MONGO_HOSTNAME = process.env.REPL
+    ? process.env.REPL_MONGO_HOSTNAME
+    : process.env.MONGO_HOSTNAME;
 
   return {
-    uri: isDevelopment
-      ? `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DATABASE}`
-      : `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DATABASE}?retryWrites=true&w=majority`,
+    uri:
+      process.env.NODE_ENV !== 'production'
+        ? `mongodb://${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DATABASE}`
+        : `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DATABASE}?retryWrites=true&w=majority`,
   };
 });
