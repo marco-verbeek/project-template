3c5d23a37eb7716ff156922c878e09a9e7473741
Marco VERBEEK <marcoverbeek99@gmail.com>
2022-07-31 20:31:49 +0200

    feat(mocks): use Faker lib to generate fake mock data

--- apps/api/src/common/mocks/entities/user.mock.ts
+++ apps/api/src/common/mocks/entities/user.mock.ts
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker';
 import { Types } from 'mongoose';
 
 import { User } from '../../../users/schemas/user.schema';
@@ -5,7 +6,8 @@ import { User } from '../../../users/schemas/user.schema';
 export const getUserMock = (): User => {
   return {
     _id: new Types.ObjectId(),
-    email: 'user.mock@example.com',
-    password: 'plain-text-pw',
+
+    email: faker.internet.email(),
+    password: faker.internet.password(),
   };
 };
